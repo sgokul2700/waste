92476de91118fbb127a69a938f9d807ec7524136
sgokul2700 <sgokul2700@gmail.com>
2019-08-27 23:03:31 +0530

    Final2

--- app/src/main/java/com/example/sairamkrishna/myapplication/MainAdmin3.java
+++ app/src/main/java/com/example/sairamkrishna/myapplication/MainAdmin3.java
@@ -2,7 +2,10 @@ package com.example.sairamkrishna.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainAdmin3 extends AppCompatActivity {
     String s1,s2,s3;
@@ -17,5 +20,12 @@ public class MainAdmin3 extends AppCompatActivity {
         s1 = ev1.getText().toString();
         s2 = ev2.getText().toString();
         s3 = ev3.getText().toString();
+        Button b1 = (Button)findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "Inserted", Toast.LENGTH_SHORT).show();
+            }});
     }
 }
