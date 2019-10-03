d5f2b212b6ba6b94a3869eb0f44d0dfb8429ae4b
Goutham <gouthams0922@gmail.com>
2019-10-03 21:37:48 +0530

    Database update - 3 --- -Added table creation of all 7 tables. -Added insertion commands for first 4 tables.

--- app/src/main/java/com/example/sairamkrishna/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/sairamkrishna/myapplication/DatabaseHelper.java
@@ -9,14 +9,21 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String databaseName = "student.db";
     public static final String area = "area";
-    public static final String nameKey = "waste_key";
-    public static final String nameWaste = "waste";
-    public static final String keycol_1 = "ID";
-    public static final String keycol_2 = "G_ID";
-    public static final String keycol_3 = "WASTE_ID";
-    public static final String col_1 = "AREA_ID";
-    public static final String col_2 = "TALUK_ID";
-    public static final String col_3 = "AREA_NAME";
+    public static final String town = "town";
+    public static final String taluk = "taluk";
+    public static final String waste = "waste";
+    public static final String suggestion = "suggestion";
+    public static final String user = "user";
+    public static final String grievance = "grievance";
+    public static final String area_id = "AREA_ID";
+    public static final String taluk_id = "TALUK_ID";
+    public static final String taluk_name = "TALUK_NAME";
+    public static final String area_name = "AREA_NAME";
+    public static final String town_id = "TOWN_ID";
+    public static final String town_name = "TOWN_NAME";
+    public static final String waste_id = "WASTE_ID";
+    public static final String waste_type = "WASTE_TYPE";
+    public static final String level = "LEVEL";
     public DatabaseHelper(Context context) {
         super(context, databaseName, null, 1);
 
@@ -24,19 +31,73 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table "+area+" (AREA_ID integer, TALUK_ID integer, AREA_NAME text) ");
-        sqLiteDatabase.execSQL("create table "+nameKey+" (ID integer, G_ID integer, WASTE_ID integer) ");
-        sqLiteDatabase.execSQL("create table "+nameWaste+" (WASTE_ID integer, WASTE_TYPE text, LEVEL integer, METHOD text, SOLUTION text) ");
-
+        sqLiteDatabase.execSQL("create table "+town+" (TOWN_ID numeric, TOWN_NAME varchar(100),primary key(TOWN_ID)) ");
+        sqLiteDatabase.execSQL("create table "+taluk+" (TALUK_ID numeric, TALUK_NAME varchar(100),TOWN_ID numeric,primary key(TALUK_ID),foreign key(TOWN_ID) references TOWN) ");
+        sqLiteDatabase.execSQL("create table "+area+" (AREA_ID numeric, AREA_NAME varchar(100),TALUK_ID numeric,TOWN_ID numeric,primary key(AREA_ID),foreign key(TALUK_ID) references TALUK, foreign key(TOWN_ID) references TOWN) ");
+        sqLiteDatabase.execSQL("create table "+waste+" (WASTE_ID numeric, WASTE_TYPE numeric, AREA_ID numeric, LEVEL numeric,primary key(WASTE_ID),foreign key(AREA_ID) references AREA) ");
+        sqLiteDatabase.execSQL("create table "+suggestion+" (LEVEL numeric, METHOD  numeric, WASTE_ID numeric, SOLUTION numeric, foreign key(WASTE_ID) references WASTE) ");
+        sqLiteDatabase.execSQL("create table "+user+" (ID numeric,PASSWORD varchar(100),NAME varchar(100),AREA_ID numeric,PHONENUMBER numeric, EMAIL_ID varchar(100),USER_TYPE varchar(100),primary key(ID), foreign key(AREA_ID) references AREA) ");
+        sqLiteDatabase.execSQL("create table "+grievance+" (G_ID numeric, AREA_ID numeric, REASON varchar(300),ID numeric,primary key(G_ID), foreign key(ID) references USER,foreign key(AREA_ID) references AREA) ");
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL("drop table if exists "+databaseName);
         onCreate(sqLiteDatabase);
     }
-    //nothing
-    public boolean insertData(String areaId, String talukId, String areaName){
+    public boolean insertTownData(String townId, String townName){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(town_id,townId);
+        contentValues.put(town_name,townName);
+        long result = sqLiteDatabase.insert(town,null,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+    public boolean insertTalukData(String talukId, String talukName, String townId){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(taluk_id,talukId);
+        contentValues.put(taluk_name,talukName);
+        contentValues.put(town_id,townId);
+        long result = sqLiteDatabase.insert(taluk,null,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+    public boolean insertAreaData(String areaId, String areaName, String talukId, String townId){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(area_id,areaId);
+        contentValues.put(area_name,areaName);
+        contentValues.put(taluk_id,talukId);
+        contentValues.put(town_id,townId);
+        //contentValues.put(col_3,areaName);
+        long result = sqLiteDatabase.insert(area,null,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean insertWasteData(String wasteId, String wasteType, String areaId, String tlevel){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(waste_id,wasteId);
+        contentValues.put(waste_type,wasteType);
+        contentValues.put(area_id,areaId);
+        contentValues.put(level,tlevel);
+        //contentValues.put(col_3,areaName);
+        long result = sqLiteDatabase.insert(area,null,contentValues);
+        if(result == -1)
+            return false;
+        else
+            return true;
+    }
+
+    /*public boolean insertData(String areaId, String talukId, String areaName){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(col_1,areaId);
@@ -47,7 +108,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         else
             return true;
-    }
+    }*/
     public Cursor getAllData(){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         Cursor res = sqLiteDatabase.rawQuery("select * from "+area,null);
